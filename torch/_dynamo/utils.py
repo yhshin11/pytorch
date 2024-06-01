d63beccac4e2c67f9270c59c8ebb98b35a9e0899
Fuzzkatt <zonghan2000@gmail.com>
2024-05-31 18:29:04 -0700

    add debug print for passing cases to compare 12.1

--- torch/_dynamo/utils.py
+++ torch/_dynamo/utils.py
@@ -1473,7 +1473,8 @@ def same(
                     multiplier = 3.0
 
                 passes_test = res_error <= (multiplier * ref_error + tol / 10.0)
-                if not passes_test:
+                # if not passes_test:
+                if True:
                     log_error(
                         "RMSE (res-fp64): %.5f, (ref-fp64): %.5f and shape=%s. res.dtype: %s, multiplier: %f, tol: %f",
                         res_error,
