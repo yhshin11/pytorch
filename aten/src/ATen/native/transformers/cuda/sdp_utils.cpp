3f1d68a1e9dcc976fdbc777210f00837cfe7790a
Eddie Yan <eddiey@nvidia.com>
2024-06-04 23:42:46 +0000

    check in

--- aten/src/ATen/native/transformers/cuda/sdp_utils.cpp
+++ aten/src/ATen/native/transformers/cuda/sdp_utils.cpp
@@ -525,7 +525,7 @@ bool can_use_cudnn_attention(const sdp_params& params, bool debug) {
           // check_cudnn_layout,
           // check_is_causal,
           check_dtypes_low_precision,
-          check_cudnn_hardware_support,
+          check_cudnn_hardware_support
 	  );
   for (auto& constraint : general_constraints) {
     if (!constraint(params, debug)) {
