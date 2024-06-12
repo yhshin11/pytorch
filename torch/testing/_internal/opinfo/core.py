2259924e2c0fac7bce8282e9abd83d9ecc9ba3af
Joel Schlosser <jbschlosser@meta.com>
2024-06-12 16:47:47 -0400

    Set seed per sample for OpInfo tests + support for restricting to a single sample input
    ghstack-source-id: 0170a5241854fcac0c03c47b7b3a6eadf4d3a1b9
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/128238

--- torch/testing/_internal/opinfo/core.py
+++ torch/testing/_internal/opinfo/core.py
@@ -28,6 +28,7 @@ from torch.testing._internal.common_dtype import (
 from torch.testing._internal.common_utils import (
     is_iterable_of_tensors,
     noncontiguous_like,
+    OPINFO_SAMPLE_INPUT_INDEX,
     TEST_WITH_ROCM,
     torch_to_numpy_dtype_dict,
     TrackedInputIter,
@@ -1182,7 +1183,11 @@ class OpInfo:
             else:
                 sample.input[0] = conjugate(sample.input[0])
 
-        return TrackedInputIter(iter(conj_samples), "conjugate sample input")
+        return TrackedInputIter(
+            iter(conj_samples),
+            "conjugate sample input",
+            restrict_to_index=OPINFO_SAMPLE_INPUT_INDEX,
+        )
 
     def sample_inputs(self, device, dtype, requires_grad=False, **kwargs):
         """
@@ -1201,7 +1206,11 @@ class OpInfo:
             samples_list.extend(conj_samples)
             samples = tuple(samples_list)
 
-        return TrackedInputIter(iter(samples), "sample input")
+        return TrackedInputIter(
+            iter(samples),
+            "sample input",
+            restrict_to_index=OPINFO_SAMPLE_INPUT_INDEX,
+        )
 
     def reference_inputs(self, device, dtype, requires_grad=False, **kwargs):
         """
@@ -1215,7 +1224,11 @@ class OpInfo:
             samples = self.sample_inputs_func(
                 self, device, dtype, requires_grad, **kwargs
             )
-            return TrackedInputIter(iter(samples), "sample input")
+            return TrackedInputIter(
+                iter(samples),
+                "sample input",
+                restrict_to_index=OPINFO_SAMPLE_INPUT_INDEX,
+            )
 
         if kwargs.get("include_conjugated_inputs", False):
             raise NotImplementedError
@@ -1223,7 +1236,11 @@ class OpInfo:
         references = self.reference_inputs_func(
             self, device, dtype, requires_grad, **kwargs
         )
-        return TrackedInputIter(iter(references), "reference input")
+        return TrackedInputIter(
+            iter(references),
+            "reference input",
+            restrict_to_index=OPINFO_SAMPLE_INPUT_INDEX,
+        )
 
     def error_inputs(self, device, **kwargs):
         """
@@ -1231,7 +1248,10 @@ class OpInfo:
         """
         errs = self.error_inputs_func(self, device, **kwargs)
         return TrackedInputIter(
-            iter(errs), "error input", callback=lambda e: e.sample_input
+            iter(errs),
+            "error input",
+            callback=lambda e: e.sample_input,
+            restrict_to_index=OPINFO_SAMPLE_INPUT_INDEX,
         )
 
     def error_inputs_sparse(self, device, layout, **kwargs):
